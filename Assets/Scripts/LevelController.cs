3dc8aa817bcdc56ef31837e03f8fa3cb24699961
GadConnorHowell <46717327+GadConnorHowell@users.noreply.github.com>
2019-02-11 22:59:08 +0000

    Initial commit

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour {
+
+    public Sprite SwitchOn;
+    public Sprite SwitchOff;
+    public GameObject Switch;
+    public GameObject Key;
+    public bool GotKey;
+    public GameObject door;
+    public Animator Anim;
+	// Use this for initialization
+	void Start () {
+
+       Anim= door.GetComponent<Animator>();
+		Switch.GetComponent<SpriteRenderer>().sprite = SwitchOff;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+
+    public void TurnSwitchOn()
+    { 
+        Switch.GetComponent<SpriteRenderer>().sprite = SwitchOn;
+		Anim.SetBool ("canOpen", true); 
+    }
+
+
+    public void TurnSwitchOff()
+    {
+        Switch.GetComponent<SpriteRenderer>().sprite = SwitchOff;
+		Anim.SetBool ("canOpen", false);
+    }
+
+
+    public void KeyCollectable()
+    {
+
+       
+        GotKey = true;
+
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public LevelController Lm;
+    public Color c;
+    public bool canTurnSwitchOn; 
+	public bool switchOn; 
+    // Use this for initialization
+    void Start () {
+        c = gameObject.GetComponent<SpriteRenderer>().color;
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        float hAxis = Input.GetAxisRaw("Horizontal");
+
+        if (hAxis == 1)
+        {
+
+
+            gameObject.transform.Translate(Vector2.right * Time.deltaTime * speed); 
+
+        }
+
+
+        if (hAxis == -1)
+        {
+
+
+            gameObject.transform.Translate(Vector2.right * Time.deltaTime * -speed);
+
+        }
+
+		if (Input.GetKeyDown(KeyCode.Space) && canTurnSwitchOn == true && Lm.GotKey == true && switchOn==false)
+        {
+
+            Lm.TurnSwitchOn();
+			switchOn = true;
+
+        }
+
+
+		else if (Input.GetKeyDown(KeyCode.Space) && canTurnSwitchOn == true  && switchOn==true)
+		{
+
+			Lm.TurnSwitchOff();
+			switchOn = false;
+
+		}
+
+
+        gameObject.GetComponent<SpriteRenderer>().color = c;
+
+
+
+
+
+    }
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Switch")
+        { 
+            c.a = 0.5f;
+            canTurnSwitchOn = true;
+        }
+    }
+
+
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Switch")
+        {
+            c.a = 1f;
+            canTurnSwitchOn = false;
+        }
+    }
+}
